7051f0b263d54aa3c4b4ea97283e39931f3b92c4
Mudassir Khan <deadsoulartist@gmail.com>
2021-08-17 17:41:30 +0530

    tab-navigator

--- App.js
+++ App.js
@@ -4,16 +4,16 @@ import { NavigationContainer } from '@react-navigation/native'
 import Home from './components/Home'
 import About from './components/About'
 
-import { createDrawerNavigator } from '@react-navigation/drawer';
-const Drawer = createDrawerNavigator();
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Drawer.Navigator initialRouteName="Home">
-        <Drawer.Screen name="Home" component={Home} />
-        <Drawer.Screen name="About" component={About} />
-      </Drawer.Navigator>
+      <Tab.Navigator initialRouteName="Home">
+        <Tab.Screen name="Home" component={Home} />
+        <Tab.Screen name="About" component={About} />
+      </Tab.Navigator>
     </NavigationContainer>
     // <View><Text>Hello world</Text></View>
   );
--- components/About.js
+++ components/About.js
@@ -2,11 +2,11 @@ import React from 'react'
 import { View, Text } from 'react-native'
 import { globalStyles } from '../globalStyles'
 
-export default function About({route,navigation}) {
-    const {name, age}=route.params
+export default function About() {
+    
     return (
         <View style={globalStyles.container}>
-            <Text>Hello from About {name}</Text>
+            <Text>Hello from About </Text>
         </View>
     )
 }
